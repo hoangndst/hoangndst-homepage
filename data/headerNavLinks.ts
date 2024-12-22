13d737f87187ae3abf0cbb584d4c31c027d70774
hoangndst <hoangndst@gmail.com>
2024-12-22 13:14:13 +0700

    feat: blog data

--- data/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
