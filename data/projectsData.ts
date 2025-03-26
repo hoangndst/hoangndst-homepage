eac4ad772dafdd1176e50ed620da0e3f444c5380
hoangndst <hoangndst@gmail.com>
2025-03-27 00:42:00 +0700

    feat: update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -31,6 +31,12 @@ const projectsData: Project[] = [
     href: 'https://viettelcloud.vn/en/products/8/30',
   },
   {
+    title: 'Ratelimit',
+    description: `Golang rate limit library.`,
+    imgSrc: '/static/images/golang.png',
+    href: 'https://github.com/hoangndst/ratelimit',
+  },
+  {
     title: 'Project Management System',
     description: `A project management system that helps teams to collaborate and manage projects from start to finish.`,
     imgSrc: 'https://raw.githubusercontent.com/hoangndst/pm/master/pm-client/public/preview.png',
@@ -56,6 +62,18 @@ const projectsData: Project[] = [
     href: 'https://github.com/hoangndst/evaluate-network-performance',
   },
   {
+    title: 'Gas Warning System',
+    description: `Gas warning system at mines using network mesh.`,
+    imgSrc: 'https://raw.githubusercontent.com/hoangndst/gas-warning-system/refs/heads/main/assets/frontend.png',
+    href: 'https://github.com/hoangndst/gas-warning-system',
+  },
+  {
+    title: 'caro-ai',
+    description: `This is a project for AI course. The goal is to create a Gomoku AI using Minimax Algorithm with Alpha-Beta Pruning and Zobrist Hashing.`,
+    imgSrc: '/static/images/golang.png',
+    href: 'https://github.com/hoangndst/caro-ai',
+  },
+  {
     title: '510Pay',
     description: `An funny online web app I made for my homies at My Dinh dormitory to manage our money.`,
     imgSrc: 'https://raw.githubusercontent.com/hoangndst/510pay/master/src/images/readme/7.png',
@@ -66,6 +84,24 @@ const projectsData: Project[] = [
     description: "This is a visualization of Kmeans algorithm. Help people to understand the algorithm and apply it to their own data.",
     imgSrc: 'https://raw.githubusercontent.com/hoangndst/kmeans-visualization/main/img/kmeans.gif',
     href: "https://github.com/hoangndst/kmeans-visualization",
+  },
+  {
+    title: "stuffops",
+    description: "All helpful container deployment and management on Docker and Kubernetes.",
+    imgSrc: '/static/images/golang.png',
+    href: "https://github.com/hoangndst/stuffops",
+  },
+  {
+    title: "@danchoicloud_bot",
+    description: "Telegram chat bot to notify, remind, send random girl images, manage tasks and more.",
+    imgSrc: '/static/images/danchoicloud_bot.png',
+    href: "https://github.com/hoangndst/danchoicloud",
+  },
+  {
+    title: "@buddy",
+    description: "Discord bot integrated with AI models, help friends about game play, remind, notify, manage tasks and more.", 
+    imgSrc: '/static/images/buddy_bot.png',
+    href: "https://github.com/hoangndst/buddy",
   }
 ]
 
