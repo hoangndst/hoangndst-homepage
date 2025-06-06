3b36a92fe1f6b3db242e51daa520e9a6cdfb357d
hoangndst <hoangndst@gmail.com>
2025-06-07 03:29:58 +0700

    chore: fix dev quotes

--- components/DevQuotes.tsx
+++ components/DevQuotes.tsx
@@ -6,21 +6,18 @@ import { useEffect, useState } from 'react'
 export default function DevQuotes() {
   const [quote, setQuote] = useState(getRandomQuote())
   useEffect(() => {
-    const interval = setInterval(() => {
-      setQuote(getRandomQuote())
-    }, 10000)
-    return () => clearInterval(interval)
+    setQuote(getRandomQuote())
   }, [])
 
   return (
     <ScrambledText
-      className="text-center text-lg font-medium text-gray-500 dark:text-gray-400 sm:text-lg/5"
+      className="break-words text-center text-lg font-medium text-gray-500 dark:text-gray-400 sm:text-lg/5"
       radius={100}
       duration={1.2}
       speed={0.5}
       scrambleChars=".:"
     >
-      <span className="break-words">{quote.text}</span>
+      <span>{quote.text}</span>
       <br />
       {quote.author}
     </ScrambledText>
